5dfb947159241ebec7c2b5bea74cb1a699fb7702
noahthomlison <noahthomlison@gmail.com>
2022-05-10 21:08:14 -0700

    creates endpoints for name and stats

--- server/index.js
+++ server/index.js
@@ -2,6 +2,18 @@ const express = require("express")
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get('/name', (req, res) => {
+  console.log("hello")
+  res.json({ message: "Hello from server!" });
+  res.send("hello")
+})
+
+app.get('/stats', (req, res) => {
+  console.log("stats endpoint")
+  res.json({ message: "Hello from server!" });
+  res.send("hello")
+})
+
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
