a449375f335702402a959ed612d1a623071ab548
noahthomlison <noahthomlison@gmail.com>
2022-05-11 19:07:10 -0700

    working to fetch summoner info from Riot api

--- server/index.js
+++ server/index.js
@@ -1,17 +1,38 @@
 const express = require("express")
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiKey = ""
+// /lol/summoner/v4/summoners/by-name/{summonerName}
+// https://developer.riotgames.com/apis#summoner-v4/GET_getBySummonerName
 
-app.get('/name', (req, res) => {
-  console.log("hello")
-  res.json({ message: "Hello from server!" });
-  res.send("hello")
+
+// https://developer.riotgames.com/apis#match-v5/GET_getMatchIdsByPUUID
+const matchURL = "https://developer.riotgames.com/apis#match-v5/GET_getMatchIdsByPUUID" 
+
+app.get('/', (req, res) => {
+  res.json({ message: "Connected" });
 })
 
-app.get('/stats', (req, res) => {
-  console.log("stats endpoint")
+app.get('/name', (req, res) => {
+  let summonerName = "cptgetdrunk"
+  const nameURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
+  console.log("name endpoint")
   res.json({ message: "Hello from server!" });
   res.send("hello")
+  fetch(nameURL, {
+  method: "GET",
+  headers:  {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Origin": "https://developer.riotgames.com",
+      "X-Riot-Token": apiKey
+  }
+})
+.then(response => response.json()) 
+.then(json => console.log(json)); 
+// .catch(err => console.log(err));
+
 })
 
 app.listen(PORT, () => {
