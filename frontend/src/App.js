53c3a1198e6b1c4a30aab0def3870f0cfcfbaa78
noahthomlison <noahthomlison@gmail.com>
2022-05-12 19:26:49 -0700

    adds front end code to control input and call api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 import {Typography, Container, Box, ThemeProvider, Button, IconButton, Grid} from '@mui/material';
+import react, {useState, useEffect} from 'react'
 
 function App() {
+  const [summoner, setSummoner] = useState()
+  const [value, setValue] = useState('');
+  const [matchData, setMatchData] = useState()
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const getData = async () => {
+    let response = await fetch('http://example.com/movies.json')
+    let data = await response.json()
+    setMatchData(data)
+  }
+
+  useEffect(() => {
+    getData(summoner)
+  }, [summoner])
+
+  // const setSummoner = () => {
+
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-      <Button></Button>
-
         <form>
-          <input placeholder='Who would you like to look up?'></input>
-          <button variant="text">Text</button>
+          <input value={value} onChange={onChange} placeholder='Who would you like to look up?'></input>
+          <button onClick={() => setSummoner(value)}variant="text">Text</button>
         </form>
       </header>
     </div>
