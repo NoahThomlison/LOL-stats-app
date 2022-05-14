57c09ae961a68125969c6957af1f3422b80f93b8
noahthomlison <noahthomlison@gmail.com>
2022-05-13 20:53:30 -0700

    connects front-end to backend, removes unneeded code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,42 +7,18 @@ function App() {
   const [summoner, setSummoner] = useState()
   const [value, setValue] = useState('');
   const [matchData, setMatchData] = useState()
-  const params = {
-    method: 'GET',
-    headers: {
-        'accept': 'application/json'
-    }
-};
+  const url = "http://localhost:3001"
   const onChange = (event) => {
     setValue(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit =  (event) => {
     event.preventDefault();
-    // getData(summoner)
-    fetch("http://localhost:3001")
-    .then(res => res.text())
-    // .then(res => this.setState({ apiResponse: res }));
-
-    // fetch('http://localhost:3001/', params)
-    // .then(response => response.json())
-    // .then(data => console.log(data));
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => console.log(res))
+      .then((data) => setMatchData(data.message));
   }
-
-  const getData = () => {
-    fetch('http://localhost:3001/', params)
-  .then(response => response.json())
-  .then(data => console.log(data));
-    // let response = await fetch('http://localhost:3001/')
-    // let data = await response.json()
-    // // let data = "test"
-    // console.log(data)
-    // // setMatchData(data)
-  }
-
-  // useEffect(() => {
-  //   getData(summoner)
-  // }, [summoner])
 
   return (
     <div className="App">
