d17e7e2166e760f5adc39ad70ef2637a3fa8488a
noahthomlison <noahthomlison@gmail.com>
2022-05-14 18:58:32 -0700

    adds code to call riot servers and get user info

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require("express")
 const PORT = process.env.PORT || 3001;
 const cors = require('cors')
 const app = express();
+const axios = require('axios').default;
 
 //configing server
 app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const apiKey = "RGAPI-a7fe9997-46bf-4a26-a959-47af274b348d"
+const apiKey = ""
 const matchURL = "https://developer.riotgames.com/apis#match-v5/GET_getMatchIdsByPUUID" 
 
 app.get("/search", (req, res) => {
@@ -18,59 +19,39 @@ app.get("/search", (req, res) => {
 
 });
 
-
 app.post("/search", (req, res) => {
-  console.log(req.body)
-  console.log("Post")
-  res.json({ message: "Post from server!" });
+  let summonerName = req.body.value
+  console.log(summonerName)
+  const nameURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
+  axios.get(nameURL, {
+    headers:  {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+      "Origin": "https://developer.riotgames.com",
+      "X-Riot-Token": apiKey
+  }
+  })
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    // response.json()
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+return
 
 });
 
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
-
-
-
-
-
-
-// app.get('/name', (req, res) => {
-//   let summonerName = "cptgetdrunk"
-//   const nameURL = `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`
-//   console.log("name endpoint")
-//   res.json({ message: "Hello from server!" });
-//   fetch(nameURL, {
-//   method: "GET",
-//   headers:  {
-//       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
-//       "Accept-Language": "en-US,en;q=0.9",
-//       "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-//       "Origin": "https://developer.riotgames.com",
-//       "X-Riot-Token": apiKey
-//   }
-// })
-// .then(response => response.json()) 
-// .then(json => console.log(json)); 
-// // .catch(err => console.log(err));
-// return
-
-// })
-
-
-// var allowCrossDomain = function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', "*");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type');
-//   next();
-// }
-
-// app.configure(function() {
-//   app.use(allowCrossDomain);
-//   //some other code
-// });  
-
-// res.set({'Access-Control-Allow-Origin': 'http://localhost:3000'})
 
 // /lol/summoner/v4/summoners/by-name/{summonerName}
 // https://developer.riotgames.com/apis#summoner-v4/GET_getBySummonerName
